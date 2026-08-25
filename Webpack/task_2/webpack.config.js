842ead62b90e38da4f7d6ccf692402bf0e8f3eea
madmeal1 <mael.salet33@gmail.com>
2026-08-25 14:11:48 +0200

    [fix] add npm install

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -26,23 +26,8 @@ module.exports = {
           {
             loader: 'image-webpack-loader',
             options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              optipng: {
-                enabled: true
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false
-              },
-              webp: {
-                quality: 75
-              }
+              // Disable heavy optimizers to avoid native binary issues in this env
+              disable: true
             }
           }
         ]
