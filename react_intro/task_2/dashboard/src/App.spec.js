ea2aa8dfbfd40e737266ebe6e0dbdde62f8fb6d3
madmeal1 <mael.salet33@gmail.com>
2026-08-26 12:21:01 +0000

    [feat] update the app to add following test

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -23,4 +23,32 @@ describe('App', () => {
     const image = screen.getByAltText(/holberton logo/i);
     expect(image).toBeInTheDocument();
   });
+
+  it('renders 2 input elements for email and password', () => {
+    const { container } = render(<App />);
+
+    const inputs = container.querySelectorAll('input');
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    expect(inputs).toHaveLength(2);
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it('renders 2 label elements with the text Email and Password', () => {
+    render(<App />);
+
+    const emailLabel = screen.getByText(/email/i);
+    const passwordLabel = screen.getByText(/password/i);
+
+    expect(emailLabel.tagName).toBe('LABEL');
+    expect(passwordLabel.tagName).toBe('LABEL');
+  });
+
+  it('renders a button with the text OK', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /ok/i });
+    expect(button).toBeInTheDocument();
+  });
 });
